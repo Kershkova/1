8c348b994cf42f786f948cffedd04865566a5129
Kershkova <124401535+Kershkova@users.noreply.github.com>
2023-02-07 23:31:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,23 +7,26 @@ middle = 0
 max = 0
 a = input("input number ")
 min = a
-while int(a) != 0:
-    i += 1
-    sum = int(sum) + int(a)
-    middle = sum / i
-    k = int(a) % 2
+if int(a) !=0:
+    while int(a) != 0:
+        i += 1
+        sum = int(sum) + int(a)
+        middle = sum / i
+        k = int(a) % 2
 
-    if k == 0:
-        even += 1
+        if k == 0:
+            even += 1
+        else:
+            odd += 1
+
+        if int(a) > int(max):
+            max = a
+        elif int(a) < int(min):
+            min = a
+
+        a = input("input number ")
     else:
-        odd += 1
-
-    if int(a) > int(max):
-        max = a
-    elif int(a) < int(min):
-        min = a
-
-    a = input("input number ")
-else:
-    print("number of inputs ", i, "\nsummary ", sum, "\naverage ", middle, "\neven numbers ", even, "\nodd numbers ", odd,"\nmax number ",max, "\nmin number", min)
-
+        print("number of inputs ", i, "\nsummary ", sum, "\naverage ", middle, "\neven numbers ", even, "\nodd numbers ", odd,"\nmax number ",max, "\nmin number", min)
+else: 
+    print ("input not 0")
+    
