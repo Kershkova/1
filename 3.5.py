2c167292c8f9a95f3d295f4a16ea1c7184178f26
Kershkova <124401535+Kershkova@users.noreply.github.com>
2023-02-08 09:33:51 +0200

    Add files via upload

---
+++ 3.5.py
@@ -0,0 +1,28 @@
+i = 0
+even = 0
+odd = 0
+k = 0
+sum = 0
+middle = 0
+max = 0
+a = input("input number ")
+min = a
+while int(a) != 0:
+    i += 1
+    sum = int(sum) + int(a)
+    middle = sum / i
+    k = int(a) % 2
+
+    if k == 0:
+        even += 1
+    else:
+        odd += 1
+
+    if int(a) > int(max):
+        max = a
+    elif int(a) < int(min):
+        min = a
+
+    a = input("input number ")
+else:
+    print("number of inputs ", i, "\nsummary ", sum, "\naverage ", middle, "\neven numbers ", even, "\nodd numbers ", odd,"\nmax number ",max, "\nmin number", min)
