2d3ef35b0b681520b4b51490e1e5355baea9569f
Kershkova <124401535+Kershkova@users.noreply.github.com>
2023-02-12 17:53:39 +0200

    Add files via upload

--- 8.py
+++ 8.py
@@ -1,3 +1,27 @@
-s = input("Введіть рядок:")
-ch = input("Введіть символ:")
-print(s.find(ch))
+s: str = input("Введіть рядок:")
+c: str = input("Введіть символ:")
+lenght = len(s)
+n = 0
+k = 0
+
+n = s.find(c)
+if n == -1:
+    print("символ відсутній")
+else:
+    print("індекси елементів: ", end="")
+    while n != -1 and k+1 < lenght - 1:
+        n = s.find(c)
+        if n != -1:
+            s = s[n+1:lenght]
+            k += n
+            print(k, end=" ")
+            k += 1
+
+
+
+
+
+
+
+
+
