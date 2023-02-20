03315bd2b97e78a5bdd30cdd056baa00ced3120b
Kershkova <124401535+Kershkova@users.noreply.github.com>
2023-02-20 17:37:25 +0200

    Add files via upload

--- 12.1.py
+++ 12.1.py
@@ -1,5 +1,5 @@
 text = input('input text: ') #ввели текст
-list_text = text.split(' ')
+list_text = set(text.split(' '))
 new_dict = {}
 for uniqie in list_text:    new_dict[uniqie] = text.count(uniqie)
 print(new_dict)
