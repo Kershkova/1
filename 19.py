56e02a9472ee436beeea35a6d11cab5d2e3b29a5
Kershkova <124401535+Kershkova@users.noreply.github.com>
2023-03-02 11:01:14 +0200

    Add files via upload

--- 19.py
+++ 19.py
@@ -2,10 +2,11 @@ import  re
 text = str(input("Введіть текст "))
 if text == "" or text.isspace():
     raise Exception("Введіть текст, який бажаєте зробити хештегом")
+text = text.title()
 text = re.sub(r'\W', '',text)
 if text == "" or text.isspace():
     raise Exception("Введіть не пустий текст, який бажаєте зробити хештегом")
-text = text.title()
+
 e = "#"
 text = e + text
 if len(text) > 140:
